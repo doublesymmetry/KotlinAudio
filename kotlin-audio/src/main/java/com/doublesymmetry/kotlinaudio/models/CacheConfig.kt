640f6d5b83e521c89e8d36f8b86f585bc50a41aa
David Chavez <david@dcvz.io>
2021-10-19 13:16:26 +0200

    Add some documentation for cache config

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/models/CacheConfig.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/models/CacheConfig.kt
@@ -1,6 +1,12 @@
 package com.doublesymmetry.kotlinaudio.models
 
+/**
+ * Configuration for cache properties of player.
+ */
 data class CacheConfig(
+    /**
+     * Maximum player cache size in kilobytes.
+     */
     val maxCacheSize: Long?,
 )
 
