819bbd23bfae30232f8d71bb647210c2b42aec08
unknown <106490582+lovegaoshi@users.noreply.github.com>
2023-06-16 05:26:56 -0700

    Update BaseAudioPlayer.kt

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/BaseAudioPlayer.kt
@@ -228,6 +228,10 @@ abstract class BaseAudioPlayer internal constructor(
 
         playerEventHolder.updateAudioPlayerState(AudioPlayerState.IDLE)
     }
+    
+    public fun getMediaSessionToken(): MediaSessionCompat.Token {
+        return mediaSession.sessionToken
+    }
 
     private fun createForwardingPlayer(): ForwardingPlayer {
         return object : ForwardingPlayer(exoPlayer) {
