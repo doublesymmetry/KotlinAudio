32093b89dcd35391ddfd41d5f36eca7397acb34b
dcvz <david@dcvz.io>
2024-02-06 22:59:12 +0100

    more syntax updates

--- kotlin-audio-example/build.gradle.kts
+++ kotlin-audio-example/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17.toString()
-        targetCompatibility = JavaVersion.VERSION_17.toString()
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "17"
